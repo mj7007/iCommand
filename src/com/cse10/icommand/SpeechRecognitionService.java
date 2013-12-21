7bdef51e81cb9b250252f631b02fbb2a0d1da932
Jayaruwan Mannapperuma <mj7007@gmail.com>
2013-12-21 08:27:20 +0530

    Error 8 in the service

--- src/com/cse10/icommand/SpeechRecognitionService.java
+++ src/com/cse10/icommand/SpeechRecognitionService.java
@@ -27,148 +27,150 @@ import android.widget.Toast;
 public class SpeechRecognitionService extends Service {
 
 	protected AudioManager mAudioManager;
-	protected SpeechRecognizer mSpeechRecognizer;
-	protected Intent mSpeechRecognizerIntent;
-	protected final Messenger mServerMessenger = new Messenger(new IncomingHandler(this));
+    protected SpeechRecognizer mSpeechRecognizer;
+    protected Intent mSpeechRecognizerIntent;
+    protected final Messenger mServerMessenger = new Messenger(new IncomingHandler(this));
 
-	protected boolean mIsListening;
-	protected volatile boolean mIsCountDownOn;
+    protected boolean mIsListening;
+    protected volatile boolean mIsCountDownOn;
 
-	@Override
-	public void onCreate() {
-		super.onCreate();
-		Toast.makeText(this, "iCommand Service started", Toast.LENGTH_SHORT).show();
-		
-		mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-		mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
-		mSpeechRecognizer.setRecognitionListener(new SpeechRecognitionListener());
-		mSpeechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-		mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-		mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, this.getPackageName());
-	}
+    @Override
+    public void onCreate() {
+            super.onCreate();
+            Toast.makeText(this, "iCommand Service started", Toast.LENGTH_SHORT).show();
+            
+            mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+            mSpeechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+            mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+            mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, this.getPackageName());
+    }
 
-	@Override
-	public int onStartCommand(Intent intent, int flags, int startId) {
-		Toast.makeText(this, "iCommand Service started", Toast.LENGTH_SHORT).show();
-		
-		int result = super.onStartCommand(intent, flags, startId);
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+            Toast.makeText(this, "iCommand Service started", Toast.LENGTH_SHORT).show();
+            
+            int result = super.onStartCommand(intent, flags, startId);
+            startListening();
+            
+            return result;
+
+    }
+
+	private void startListening() {
 		mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(getApplicationContext());
-		SpeechRecognitionListener listener = new SpeechRecognitionListener();
-		mSpeechRecognizer.setRecognitionListener(listener);
+		mSpeechRecognizer.setRecognitionListener(new SpeechRecognitionListener());
 		mSpeechRecognizer.startListening(RecognizerIntent.getVoiceDetailsIntent(getApplicationContext()));
-		
-		return result;
-
 	}
 
-	@Override
-	public void onDestroy() {
+    @Override
+    public void onDestroy() {
 
-		Toast.makeText(getApplicationContext(), "iCommand Service destroyed", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "iCommand Service destroyed", Toast.LENGTH_LONG).show();
 
-		if (mIsCountDownOn) {
-			mNoSpeechCountDown.cancel();
-		}
-		if (mSpeechRecognizer != null) {
-			mSpeechRecognizer.destroy();
-		}
-	}
+            if (mIsCountDownOn) {
+                    mNoSpeechCountDown.cancel();
+            }
+            if (mSpeechRecognizer != null) {
+                    mSpeechRecognizer.destroy();
+            }
+    }
 
-	@Override
-	public IBinder onBind(Intent arg) {
-		return null;
-	}
+    @Override
+    public IBinder onBind(Intent arg) {
+            return null;
+    }
 
-	protected CountDownTimer mNoSpeechCountDown = new CountDownTimer(5000, 5000) {
+    protected CountDownTimer mNoSpeechCountDown = new CountDownTimer(5000, 5000) {
 
-		@Override
-		public void onTick(long millisUntilFinished) {
-		}
+        @Override
+        public void onTick(long millisUntilFinished) {
+        }
 
-		@Override
-		public void onFinish() {
-			mIsCountDownOn = false;
-			Message message = Message.obtain(null, Constants.MSG_RECOGNIZER_CANCEL);
-			try {
-				mServerMessenger.send(message);
-				message = Message.obtain(null, Constants.MSG_RECOGNIZER_START_LISTENING);
-				mServerMessenger.send(message);
-			} catch (RemoteException e) {
+        @Override
+        public void onFinish() {
+                mIsCountDownOn = false;
+                Message message = Message.obtain(null, Constants.MSG_RECOGNIZER_CANCEL);
+                try {
+                        mServerMessenger.send(message);
+                        message = Message.obtain(null, Constants.MSG_RECOGNIZER_START_LISTENING);
+                        mServerMessenger.send(message);
+                } catch (RemoteException e) {
 
-			}
-		}
-	};
+                }
+        }
+    };
 
 	protected class SpeechRecognitionListener implements RecognitionListener {
 
 		@Override
-		public void onBeginningOfSpeech() {
-			// speech input will be processed, so there is no need for count
-			// down anymore
-			if (mIsCountDownOn) {
-				mIsCountDownOn = false;
-				mNoSpeechCountDown.cancel();
-			}
-		}
+        public void onBeginningOfSpeech() {
+                // speech input will be processed, so there is no need for count
+                // down anymore
+                if (mIsCountDownOn) {
+                        mIsCountDownOn = false;
+                        mNoSpeechCountDown.cancel();
+                }
+        }
 
-		@Override
-		public void onBufferReceived(byte[] buffer) {}
+        @Override
+        public void onBufferReceived(byte[] buffer) {}
 
-		@Override
-		public void onEndOfSpeech() {}
+        @Override
+        public void onEndOfSpeech() {}
 
-		@Override
-		public void onError(int error) {
-			if (mIsCountDownOn) {
-				mIsCountDownOn = false;
-				mNoSpeechCountDown.cancel();
-			}
-			mIsListening = false;
-			Message message = Message.obtain(null, Constants.MSG_RECOGNIZER_START_LISTENING);
-			
-			try {
-				mServerMessenger.send(message);
-			} catch (RemoteException e) {
+        @Override
+        public void onError(int error) {
+                if (mIsCountDownOn) {
+                        mIsCountDownOn = false;
+                        mNoSpeechCountDown.cancel();
+                }
+                mIsListening = false;
+                Message message = Message.obtain(null, Constants.MSG_RECOGNIZER_START_LISTENING);
+                
+                try {
+                        mServerMessenger.send(message);
+                } catch (RemoteException e) {
 
-			}
-			Toast.makeText(getApplicationContext(), "error" + error, Toast.LENGTH_SHORT).show();
-		}
+                }
+                Toast.makeText(getApplicationContext(), "error" + error, Toast.LENGTH_SHORT).show();
+                startListening();
+        }
 
-		@Override
-		public void onEvent(int eventType, Bundle params) {
+        @Override
+        public void onEvent(int eventType, Bundle params) {
 
-		}
+        }
 
-		@Override
-		public void onPartialResults(Bundle partialResults) {
+        @Override
+        public void onPartialResults(Bundle partialResults) {
 
-		}
+        }
 
-		@Override
-		public void onReadyForSpeech(Bundle params) {
-			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-				mIsCountDownOn = true;
-				mNoSpeechCountDown.start();
-				mAudioManager.setStreamMute(AudioManager.STREAM_SYSTEM, false);
-			}
-		}
+        @Override
+        public void onReadyForSpeech(Bundle params) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                        mIsCountDownOn = true;
+                        mNoSpeechCountDown.start();
+                        mAudioManager.setStreamMute(AudioManager.STREAM_SYSTEM, false);
+                }
+        }
 
-		@Override
-		public void onResults(Bundle results) {
-			
-			ArrayList<String> strlist = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-			for (int i = 0; i < strlist.size(); i++) {
-				Log.d("Speech", "result=" + strlist.get(i));
-			}
-			processCommand(strlist);
-			
-			mSpeechRecognizer.startListening(RecognizerIntent.getVoiceDetailsIntent(getApplicationContext()));
-		}
+        @Override
+        public void onResults(Bundle results) {
+                
+                ArrayList<String> strlist = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                for (int i = 0; i < strlist.size(); i++) {
+                        Log.d("Speech", "result=" + strlist.get(i));
+                }
+                processCommand(strlist);
+                
+                startListening();
+        }
 
-		@Override
-		public void onRmsChanged(float rmsdB) {
+        @Override
+        public void onRmsChanged(float rmsdB) {
 
-		}
+        }
 		
 		private void processCommand(ArrayList<String> matches) {
 			if(matches != null) {
